354bb3da040aa0b064eb6d24663fe071557d4f00
MaratFa <evolution99@rambler.ru>
2024-04-16 01:14:45 +0300

    Edited

--- live-vanilla-build/js/app.js
+++ live-vanilla-build/js/app.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector(".menu");
+const menuItems = menu.querySelector(".items");
 
 menu.addEventListener("click", (event) => {
-  console.log(event.target);
+  menuItems.classList.toggle("hidden");
 });
