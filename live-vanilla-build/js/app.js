e2fa4aeb5c580d6602dbbda0183bfee056fa4a0f
MaratFa <evolution99@rambler.ru>
2024-04-18 08:13:38 +0300

    Edited

--- live-vanilla-build/js/app.js
+++ live-vanilla-build/js/app.js
@@ -1,6 +1,17 @@
-const menu = document.querySelector(".menu");
-const menuItems = menu.querySelector(".items");
+const App = {
+  // All of our selected HTML elements
+  $: {
+    menu: document.querySelector('[data-id="menu"]'),
+    menuItems: document.querySelector('[data-id="menu-items"]'),
+    resetBtn: document.querySelector('[data-id="reset-btn"]'),
+    newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
+  },
 
-menu.addEventListener("click", (event) => {
-  menuItems.classList.toggle("hidden");
-});
+  init() {
+    App.$.menu.addEventListener("click", () => {
+      App.$.menuItems.classList.toggle("hidden");
+    });
+  },
+};
+
+window.addEventListener("load", App.init);
