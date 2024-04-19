2f1bbe2b5b99f2461bb9aa29b4c21886c755834a
MaratFa <evolution99@rambler.ru>
2024-04-19 04:27:50 +0300

    Edited

--- live-vanilla-build/js/app.js
+++ live-vanilla-build/js/app.js
@@ -5,11 +5,34 @@ const App = {
     menuItems: document.querySelector('[data-id="menu-items"]'),
     resetBtn: document.querySelector('[data-id="reset-btn"]'),
     newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
+    squares: document.querySelectorAll('[data-id="square"'),
   },
 
   init() {
+    App.registerEventListeners();
+  },
+
+  registerEventListeners() {
+    // DONE 
     App.$.menu.addEventListener("click", () => {
       App.$.menuItems.classList.toggle("hidden");
+    });
+
+    // TODO
+    App.$.resetBtn.addEventListener("click", () => {
+      console.log("Reset the game");
+    });
+
+    // TODO
+    App.$.newRoundBtn.addEventListener("click", () => {
+      console.log("New");
+    });
+
+    // TODO
+    App.$.squares.forEach((square) => {
+      square.addEventListener("click", (event) => {
+        console.log(event.target.id);
+      });
     });
   },
 };
