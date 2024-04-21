c7f2cdec0c7f1ed2a661f7ef2183a5c998def05e
MaratFa <evolution99@rambler.ru>
2024-04-21 12:20:00 +0300

    Edited

--- live-vanilla-build/js/app.js
+++ live-vanilla-build/js/app.js
@@ -8,6 +8,10 @@ const App = {
     squares: document.querySelectorAll('[data-id="square"'),
   },
 
+  state: {
+    moves: [],
+  },
+
   init() {
     App.registerEventListeners();
   },
@@ -31,15 +35,45 @@ const App = {
     // TODO
     App.$.squares.forEach((square) => {
       square.addEventListener("click", (event) => {
-        console.log(event.target.id);
+        // Check if there is already a play, if so, return early
+        if (square.hasChildNodes()) {
+          return;
+        }
 
+        // Determine which player icon to add to the square
+        const lastMove = App.state.moves.at(-1);
+        const currentPlayer =
+          App.state.moves.length === 0 ? 1 : lastMove.playerId;
         const icon = document.createElement("i");
-        icon.classList.add("fa-solid", "fa-x", "yellow");
 
-        event.target.replaceChildren(icon);
+        if (currentPlayer === 1) {
+          icon.classList.add("fa-solid", "fa-x", "yellow");
+        } else {
+          icon.classList.add("fa-solid", "fa-o", "turquoise");
+        }
 
-        // <i class="fa-solid fa-o turquoise"></i>
-        // <i class="fa-solid fa-o turquoise"></i>
+        App.state.moves.push({
+          squareId: square.id,
+          playerId: currentPlayer,
+        });
+
+        App.state.currentPlayer = App.state.currentPlayer === 1 ? 2 : 1;
+
+        console.log(App.state);
+
+        square.replaceChildren(icon);
+
+        // Check if there is a winner or tie game
+        const winningPatterns = [
+          [1, 2, 3],
+          [1, 5, 9],
+          [1, 4, 7],
+          [2, 5, 8],
+          [3, 5, 7],
+          [3, 6, 9],
+          [4, 5, 6],
+          [7, 8, 9],
+        ];
       });
     });
   },
