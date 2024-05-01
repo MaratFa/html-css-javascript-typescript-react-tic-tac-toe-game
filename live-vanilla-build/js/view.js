35baadf58c7ea11397a406750ea5a803e21e1a56
MaratFa <evolution99@rambler.ru>
2024-05-02 00:14:59 +0300

    Edited

--- live-vanilla-build/js/view.js
+++ live-vanilla-build/js/view.js
@@ -8,11 +8,14 @@ export default class View {
     this.$.menuItems = this.#qs('[data-id="menu-items"]');
     this.$.resetBtn = this.#qs('[data-id="reset-btn"]');
     this.$.newRoundBtn = this.#qs('[data-id="new-round-btn"]');
-    this.$.squares = this.#qsAll('[data-id="square"');
+    this.$.squares = this.#qsAll('[data-id="square"]');
     this.$.modal = this.#qs('[data-id="modal"]');
     this.$.modalText = this.#qs('[data-id="modal-text"]');
     this.$.modalBtn = this.#qs('[data-id="modal-btn"]');
     this.$.turn = this.#qs('[data-id="turn"]');
+    this.$.p1Wins = this.#qs('[data-id="p1-wins"]');
+    this.$.p2Wins = this.#qs('[data-id="p2-wins"]');
+    this.$.ties = this.#qs('[data-id="ties"]');
 
     // Elements lists
     this.$$.squares = this.#qsAll('[data-id="square"');
@@ -46,7 +49,11 @@ export default class View {
     DOM helper methods
   */
 
-    
+  updateScoreboard(p1Wins, p2Wins, ties) {
+    this.$.p1Wins.innerText = `${p1Wins} wins`;
+    this.$.p2Wins.innerText = `${p2Wins} wins`;
+    this.$.ties.innerText = `${ties} ties`;
+  }
 
   openModal(message) {
     this.$.modal.classList.remove("hidden");
