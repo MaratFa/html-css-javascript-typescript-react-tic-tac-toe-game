39217489e276d58ce1b3aa67505d493495ddd491
MaratFa <evolution99@rambler.ru>
2024-05-04 22:18:00 +0300

    Edited

--- live-vanilla-build/js/app.js
+++ live-vanilla-build/js/app.js
@@ -20,18 +20,6 @@ function init() {
   const view = new View();
   const store = new Store("live-t3-storage-key", players);
 
-  // function initView() {
-  //   view.closeAll();
-  //   view.clearMoves();
-  //   view.setTurnIndicator(store.game.currentPlayer);
-  //   view.updateScoreboard(
-  //     store.stats.playerWithStats[0].wins,
-  //     store.stats.playerWithStats[1].wins,
-  //     store.stats.ties
-  //   );
-  //   view.initializeMoves(store.game.moves);
-  // }
-
   window.addEventListener("storage", () => {
     console.log("State changed from another tab");
     view.render(store.game, store.stats);
@@ -58,24 +46,10 @@ function init() {
       return;
     }
 
-    // Place an icon of the current player in a square
-    view.handlePlayerMove(square, store.game.currentPlayer);
-
     // Advance to the next state by pushing a move to the moves array
     store.playerMove(+square.id);
 
-    if (store.game.status.isComplete) {
-      view.openModal(
-        store.game.status.winner
-          ? `${store.game.status.winner.name} wins!`
-          : "Tie!"
-      );
-
-      return;
-    }
-
-    // Set the next player's turn indicator
-    view.setTurnIndicator(store.game.currentPlayer);
+    view.render(store.game, store.stats);
   });
 }
 
